786e6a5d7d0307dc4477d9c181cf86ac88236e7c
mikansc <michael.nsc@outlook.com>
2021-02-25 15:28:32 -0300

    refactor: change routes strategy

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,10 @@
-import { BrowserRouter, Route } from "react-router-dom";
-
-import GlobalStyles from "./styles/GlobalStyles";
-
-import Header from "./components/Header";
 import List from "./pages/List";
 import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 
-const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Route path="/" exact component={List} />
-      <Route path="/products" exact component={List} />
-      <Route path="/products/:id" component={Details} />
-      <Route path="/cart" component={Cart} />
-      <GlobalStyles />
-    </BrowserRouter>
-  );
-};
-
-export default Routes;
+export const routes = [
+  { path: "/", component: List, exact: true },
+  { path: "/products", component: List, exact: true },
+  { path: "/products/:id", component: Details, exact: false },
+  { path: "/cart", component: Cart, exact: false },
+];
