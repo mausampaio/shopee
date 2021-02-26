deec40bdbf7a2ab8ef35ec9232aa67f949afae58
Cristian Ogliari <cogliari@gmail.com>
2021-02-25 21:18:47 -0300

    refactor: add empty cart  message

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -6,37 +6,43 @@ import { CartItem } from "./CartItem";
 import { Content, Header, Footer, TotalPrice } from "./styles";
 
 const Cart = () => {
-  const state = React.useContext(Store);
-  const { cart } = state;
+  const context = React.useContext(Store);
+  const { cart, addProduct, removeProduct, getCartTotal } = context;
 
   return (
     <Container>
-      <h1>Titulo da Pagina</h1>
-      <div>
-        <Header>
-          <div></div>
-          <span>Preco Un</span>
-          <span>Quantidade</span>
-          <span>Subtotal</span>
-        </Header>
-        <Content>
-          {cart?.map((product, index) => (
-            <CartItem
-              key={product.id}
-              product={product}
-              lineBreak={cart.length !== index + 1}
-            />
-          ))}
-        </Content>
-        <Footer>
-          <div>
-            <TotalPrice>{localePriceFormater(590.0)}</TotalPrice>
-          </div>
-          <div style={{ width: "226px" }}>
-            <Button>Finalizar Compra</Button>
-          </div>
-        </Footer>
-      </div>
+      <h1>Carrinho</h1>
+        {cart.length === 0 
+          ? (<h2 style={{display: "flex", justifyContent : "center"}}>Não existem produtos</h2>) 
+          : (
+        <div>
+          <Header>
+            <div></div>
+            <span>Preco Un</span>
+            <span>Quantidade</span>
+            <span>Subtotal</span>
+          </Header>
+          <Content>
+            {cart?.map((product, index) => (
+              <CartItem
+                key={product.id}
+                product={product}
+                lineBreak={cart.length !== index + 1}
+                onIncrement={addProduct}
+                onDecrement={removeProduct}
+              />
+            ))}
+          </Content>
+          <Footer>
+            <div>
+              <TotalPrice>{localePriceFormater(getCartTotal())}</TotalPrice>
+            </div>
+            <div style={{ width: "226px" }}>
+              <Button>Finalizar Compra</Button>
+            </div>
+          </Footer>
+        </div>
+        )}
     </Container>
   );
 };
