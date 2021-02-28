e059a873a953accabc70e04a50d2ec1f61177bda
Mauricio Sampaio <mausampaio@gmail.com>
2021-02-28 16:57:33 -0300

    refactor: change alert to toastify on cart page

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { toast } from 'react-toastify';
+import { Loading } from "../../components/Loading";
 import { Store } from "../../context";
 import { Button, Container } from "../../styles/GlobalStyles";
 import { priceFormatter } from "../../utils/priceFormatter";
@@ -6,62 +8,80 @@ import { CartItem } from "./CartItem";
 import { Content, Header, Footer, TotalPrice } from "./styles";
 
 const Cart = () => {
+  const showAlert = () => new Promise(resolve => {
+    const sucess = () => {
+      toast.success("A compra foi processada com sucesso, Obrigado!", { position: "bottom-center", });
+      resolve();
+    };
+    setTimeout(sucess, 2500);
+  });
   const context = React.useContext(Store);
   const {
     cart,
     setCart,
+    loading,
+    setLoading,
     addProduct,
     removeProduct,
     getCartTotal,
     removeCartFromLocalStorage,
   } = context;
 
-  const checkout = () => {
+  const checkout = async () => {
+    setLoading(true);
     setCart([]);
     removeCartFromLocalStorage();
-    alert("A compra foi processada com sucesso, Obrigado!");
+    await showAlert();
+    setLoading(false);
   };
 
-  return (
-    <Container>
-      <h1 style={{ margin: "32px 0 36px" }}>Carrinho</h1>
-      {cart.length === 0 ? (
-        <h2 style={{ display: "flex", justifyContent: "center" }}>
-          Não existem produtos
-        </h2>
-      ) : (
-        <div>
-          <Header>
-            <div></div>
-            <span>Preço UN.</span>
-            <span>Quantidade</span>
-            <span>Subtotal</span>
-          </Header>
-          <Content>
-            {cart?.map((product, index) => (
-              <CartItem
-                key={product.id}
-                product={product}
-                lineBreak={cart.length !== index + 1}
-                onIncrement={addProduct}
-                onDecrement={removeProduct}
-              />
-            ))}
-          </Content>
-          <Footer>
-            <div>
-              <TotalPrice>
-                {priceFormatter({ price: getCartTotal(), formatCents: false })}
-              </TotalPrice>
-            </div>
-            <div style={{ width: "226px" }}>
-              <Button onClick={checkout}>Finalizar Compra</Button>
-            </div>
-          </Footer>
-        </div>
-      )}
-    </Container>
-  );
+  if (loading) {
+    return (
+      <Loading />
+    );
+  } else {
+    return (
+      <Container>
+        <h1 style={{ margin: "32px 0 36px" }}>Carrinho</h1>
+        {cart.length === 0 ? (
+          <h2 style={{ display: "flex", justifyContent: "center" }}>
+            Não existem produtos
+          </h2>
+        ) : (
+          <div>
+            <Header>
+              <div></div>
+              <span>Preço UN.</span>
+              <span>Quantidade</span>
+              <span>Subtotal</span>
+            </Header>
+            <Content>
+              {cart?.map((product, index) => (
+                <CartItem
+                  key={product.id}
+                  product={product}
+                  lineBreak={cart.length !== index + 1}
+                  onIncrement={addProduct}
+                  onDecrement={removeProduct}
+                />
+              ))}
+            </Content>
+            <Footer>
+              <div>
+                <TotalPrice>
+                  {priceFormatter({ price: getCartTotal(), formatCents: false })}
+                </TotalPrice>
+              </div>
+              <div style={{ width: "226px" }}>
+                <Button onClick={checkout}>Finalizar Compra</Button>
+              </div>
+            </Footer>
+          </div>
+        )}
+      </Container>
+    );
+  }
+
 };
 
 export default Cart;
