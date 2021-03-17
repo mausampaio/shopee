c7fcd939db83c4efbbcb81443bf4c327fab05201
mikansc <michael.nsc@outlook.com>
2021-03-16 23:17:46 -0300

    test: test Container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 80%;
+  max-width: 1280px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 90%;
+  }
+`;
