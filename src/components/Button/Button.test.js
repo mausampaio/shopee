383d81cee1620d259b630af85836cb7c4a943156
mikansc <michael.nsc@outlook.com>
2021-03-16 23:16:41 -0300

    refactor: change Button component inner text strategy

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -3,13 +3,13 @@ import { render, fireEvent } from "../../test-utils";
 import { Button } from "./index";
 
 test("should match snapshot", () => {
-  const { asFragment } = render(<Button>text</Button>);
+  const { asFragment } = render(<Button>Button</Button>);
   expect(asFragment()).toMatchSnapshot();
 });
 
 test("should call an onClick event when pressed", () => {
   const handleClick = jest.fn();
-  const { getByRole } = render(<Button onClick={handleClick}>text</Button>);
+  const { getByRole } = render(<Button onClick={handleClick} />);
   fireEvent.click(getByRole("button"));
   expect(handleClick).toBeCalledTimes(1);
 });
